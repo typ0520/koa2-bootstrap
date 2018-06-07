68e416ab078164b1fceb3174663aaeb2a024e823
typ0520 <yapeng.tong@baidao.comc>
2018-06-07 10:34:46 +0800

    add dynamic params

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -10,4 +10,9 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.get('/:id', function (ctx, next) {
+  console.log(ctx.params.id)
+  ctx.body = 'this is a users/:id response!'
+})
+
 module.exports = router
